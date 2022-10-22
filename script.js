aaf83de18a6131642cf0c123f56435d11834c6f6
tosmel2 <adewaletosin0808@gmail.com>
2022-10-22 01:01:59 +0100

    dx antic remastered hamburger fixed

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // Mobile responsive menu
-
 const menu = document.getElementById("barbox");
 const view =document.querySelector(".nav__Mobilelinks");
 
 function controller() {
     view.classList.toggle("show");
-    backdrop.classList.toggle("show");
     menu.classList.toggle("change")
 }
 
