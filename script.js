7fa0f840e02c2b03602ec9475bddec7cfc32950d
tosmel2 <adewaletosin0808@gmail.com>
2022-10-31 17:05:08 +0100

    application of slider images

--- script.js
+++ script.js
@@ -11,3 +11,23 @@ menu.addEventListener("click", function (e) {
     e.preventDefault()
     controller();
 })
+
+
+// slider images
+const productContainers = [...document.querySelectorAll('.product-image')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+    
+});
